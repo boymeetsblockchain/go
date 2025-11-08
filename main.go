3256c72521d2500e1fc5cdc7b52419bb512d98d8
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-11-08 22:39:43 +0100

    chore:stopped here for now

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Movie struct {
 	ID       string    `json:"id"`
@@ -14,6 +20,20 @@ type Director struct {
 	Lastname  string `json:"lastname"`
 }
 
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "1234848", Title: "Movie One", Director: &Director{Firstname: "pablo", Lastname: "mari"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "1234848", Title: "Movie Two", Director: &Director{Firstname: "pablo", Lastname: "mari the second"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
