8965970f8f499141d5807e98407e7afd72997e9f
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-11-10 12:01:55 +0100

    feat:added the model, routes and utils file

---
+++ pkg/routes/bookstores-routes.go
@@ -0,0 +1,15 @@
+package route
+
+import (
+	"github.com/boymeetsblockchain/basic-go-sever/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+
+}
